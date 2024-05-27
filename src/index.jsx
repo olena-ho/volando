0c282905ad8824f6bb5a4ccf824cbd702ffd8afb
Viktoriia Nykyforova <113258083+ViktoriiaKN@users.noreply.github.com>
2024-05-27 19:52:48 +0200

    Router, App, deleted default code

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,25 @@
+import React, { Children } from "react";
 import { createRoot } from 'react-dom/client';
-import { HomePage } from './pages/HomePage';
+import { createBrowserRouter, RouterProvider} from "react-router-dom";
+import { App } from "./components/App";
+import { HomePage } from "./pages/HomePage";
+import './i18n';
 import './global.css';
 
-createRoot(
-  document.querySelector('#app'),
-).render(<HomePage />);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.querySelector("#app")).render(
+  <RouterProvider router={router} />
+);
