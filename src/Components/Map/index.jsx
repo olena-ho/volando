ecce91b375d17a6116441e68a0a4eb0ca88dc0e4
Viktoriia Nykyforova <113258083+ViktoriiaKN@users.noreply.github.com>
2024-05-28 04:29:53 +0200

    current location Map

--- src/Components/Map/index.jsx
+++ src/Components/Map/index.jsx
@@ -6,7 +6,7 @@ import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
   height: '100%'
 }; 
 
-const center = {
+const defaultCenter = {
   lat: 49.5,
   lng: 14.5
 };
@@ -20,17 +20,39 @@ const handleMapLoad = (map) => {
 };
 
 const Map = () => {
+  const [currentLocation, setCurrentLocation] = useState(null);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setCurrentLocation({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          });
+        },
+        (error) => {
+          console.error("Error getting user's location", error);
+          setCurrentLocation(defaultCenter);
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+      setCurrentLocation(defaultCenter);
+    }
+  }, []);
+
   return (
     <>
     <LoadScript googleMapsApiKey="AIzaSyBiJG97_IYoMHOFyLB-JmGfXWGQa9ocJ24"
     onError={handleLoadError}>
       <GoogleMap
         mapContainerStyle={containerStyle}
-        center={center}
+        center={currentLocation || defaultCenter}
         zoom={10}
         onLoad={handleMapLoad}
       >
-        { /* Add any child components or markers here */ }
+        {currentLocation && <Marker position={currentLocation} />}
       </GoogleMap>
       </LoadScript>
       </>
