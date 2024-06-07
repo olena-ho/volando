69b61a7ebd4948ebd8d21ac887c830b87f610f48
Olena Horovenko <elena.gorovenko@gmail.com>
2024-06-07 13:55:11 +0200

    added text to HomeVisual

--- src/Components/HomeVisual/index.jsx
+++ src/Components/HomeVisual/index.jsx
@@ -1,14 +1,28 @@
 import snowboarder from "./img/snowboarder.jpg";
 import geolocation from "./img/geolocation.png";
 import traveler from "./img/traveler.png";
+import { useTranslation } from "react-i18next";
 import "./style.css";
 
-export const HomeVisual = () => { 
+export const HomeVisual = () => {
+  const { t, i18n } = useTranslation();
   return (
     <div className="main-page__visual">
-      <img src={snowboarder} alt="snowboarder" className="main-page__image" />  
-      <img src={geolocation} alt="pick your location" className="main-page__image main-page__image--s" />  
-      <img src={traveler} alt="go on vacation" className="main-page__image" />  
+      <div className="visual-images">
+        <img src={snowboarder} alt="snowboarder" className="main-page__image" />
+        <img
+          src={geolocation}
+          alt="pick your location"
+          className="main-page__image main-page__image--s"
+        />
+        <img src={traveler} alt="go on vacation" className="main-page__image" />
+      </div>
+
+      <div className="visual-text">
+        <p className="visual-text__item">{t("home-text1")}</p>
+        <p className="visual-text__item">{t("home-text2")}</p>
+        <p className="visual-text__item">{t("home-text3")}</p>
+      </div>
     </div>
   );
-}
+};
