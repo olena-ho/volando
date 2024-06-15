8d68e117afdab419e1281fd55e29bc2c7cd5bee6
Olena Horovenko <elena.gorovenko@gmail.com>
2024-06-15 17:03:08 +0200

    added index.jsx to App import

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider} from "react-router-dom";
-import { App } from "./components/App";
+import { App } from "./components/App/index.jsx";
 import { HomePage } from "./pages/HomePage";
 import './i18n';
 import './variables.css';
